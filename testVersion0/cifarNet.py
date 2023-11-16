a6114304e4f9dd1ceb5b0d864aae76477249d1ee
jeanneLxy <123762147+jeanneLxy@users.noreply.github.com>
2023-11-17 02:32:06 +0800

    Update cifarNet.py
    ADD torch.transforms---pretraitement
    ADD imbalanced-learn packet---over-sampling label 1

--- testVersion0/cifarNet.py
+++ testVersion0/cifarNet.py
@@ -1,36 +1,30 @@
-import time
 
 import torch
+from imblearn.over_sampling import RandomOverSampler
 from sklearn.metrics import classification_report
 from torch import nn
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, WeightedRandomSampler
 from torch.utils.data import DataLoader
 import numpy as np
 from torch.utils.tensorboard import SummaryWriter
-from torchvision import models
-
+from torchvision import models, transforms
+# Ignore warnings
+import warnings
+warnings.filterwarnings("ignore")
 
 class MyDataset(Dataset):
-    def __init__(self,filepath):
-
-        xy=np.loadtxt(filepath, delimiter=',',dtype=np.float32)
-
-        #total length
-        self.len = xy.shape[0]
-
-        self.x=xy[:,1:]
-
-        self.x=torch.from_numpy(self.x)
-        print(f"read data= {self.x}")
-        print(f"read data shape= {self.x.shape}")
-
-        #create y_label
-        self.y = xy[:, [0]]
-        self.y=torch.from_numpy(self.y)
-        self.y=torch.flatten(self.y)
-        # print(f"read label = {self.y}")
-        print(f"read label shape= {self.y.shape}")
-
+    def __init__(self, x=None, y=None, filepath=None,transforms=None):
+        if filepath:
+            xy = np.loadtxt(filepath, delimiter=',', dtype=np.float32)
+            self.x = torch.from_numpy(xy[:, 1:])
+            self.y = torch.from_numpy(xy[:, [0]])
+            self.y = torch.flatten(self.y)
+        else:
+            self.x = x
+            self.y = y
+        self.len = self.x.shape[0] if self.x is not None else 0
+        # total length
+        print(f"length return= {self.len}")
         # find the right label order
         self.res = []
         self.list=self.y.numpy()
@@ -42,52 +36,70 @@ class MyDataset(Dataset):
         print(f"class = {self.res}")
         print("Data prepared--------------------------------------------------------------------")
 
+        # Set the transform for data augmentation
+        self.transforms = transforms
+
     #prepare index: dataset[index]
     def __getitem__(self, index):
-        return self.x[index],self.y[index],self.res
+        x,y= self.x[index],self.y[index]
 
+        # Reshape the flattened data to the original image shape
+        x = x.view(3, 74, 74)  # Assuming images are 3-channel and size 74x74, adjust accordingly
+        # Apply data augmentation only if the label is 1
+        if y == 1 and self.transforms is not None:
+            x = self.transforms(x)
+
+        return x,y
     #prepare length: len(dataset)
     def __len__(self):
         return self.len
 
-train_file="train.csv"
-train_data=MyDataset(train_file)
-train_lst=train_data.res
-train_loader=DataLoader(dataset=train_data,
-                        batch_size=20,
-                        shuffle=True,
-                        drop_last=True)
+# Define data augmentation transform
+augmentation_transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(degrees=15),
+    # Add other data augmentation operations
+    transforms.ToTensor(),
+])
+
+
+# 创建一个函数来对少数类别进行过采样
+def oversample_data(dataset):
+    labels = dataset.y.numpy()
+
+    # 使用 RandomOverSampler 对少数类别进行过采样
+    oversampler = RandomOverSampler(sampling_strategy='minority', random_state=42)
+    x_resampled, y_resampled = oversampler.fit_resample(dataset.x.numpy(), labels)
+
+    # 转换为 PyTorch 张量
+    x_resampled = torch.from_numpy(x_resampled)
+    y_resampled = torch.from_numpy(y_resampled)
+    print(f"x_reshaped={x_resampled.shape}")
+
+    # 使用过采样后的数据创建新的数据集
+    oversampled_dataset = MyDataset(x=x_resampled, y=y_resampled,filepath=None)
+    return oversampled_dataset
+
+
+# 使用 oversample_data 函数获取过采样后的数据集
+train_file = "train (2).csv"
+train_data_origin = MyDataset(filepath=train_file, transforms=augmentation_transform)
+train_data = oversample_data(train_data_origin)
+train_loader = DataLoader(dataset=train_data,
+                          batch_size=20,
+                          shuffle=True,
+                          drop_last=True)
+
+
 test_file="test.csv"
-test_data=MyDataset(test_file)
+test_data=MyDataset(filepath=test_file)
 test_lst=test_data.res
 test_loader=DataLoader(dataset=test_data,
                         batch_size=20,
                         shuffle=True,
                         drop_last=True)
 
-# # construction of model
-# class CIFAR(nn.Module):
-#     def __init__(self,class_num):
-#         super(CIFAR, self).__init__()
-#         self.class_num=class_num
-#         self.model = nn.Sequential(
-#             nn.Conv2d(3, 32, 5, 1, 2),
-#             nn.MaxPool2d(2),
-#             nn.Conv2d(32, 32, 5, 1, 2),
-#             nn.MaxPool2d(2),
-#             nn.Conv2d(32, 64, 5, 1, 2),
-#             nn.MaxPool2d(2),
-#             nn.Flatten(),
-#             nn.Linear(5184,64),
-#             nn.Linear(64,self.class_num)
-#         )
-#
-#     def forward(self, x):
-#         x = self.model(x)
-#         return x
-#
-# #Create model
-# cifar_train=CIFAR(len(train_lst))
 
 Use_gpu = torch.cuda.is_available()
 model = models.vgg16(pretrained=True)
@@ -111,22 +123,22 @@ if Use_gpu:
 
 
 #Loss Fonction & Modulation of weight
-weights = [1, 100000]
+weights = [1, 100]
 class_weights = torch.FloatTensor(weights).to()
 loss_func = nn.CrossEntropyLoss(weight=class_weights)
 
 
 #Learning rate=0.01
-learning_rate=1e-2
+learning_rate=1e-3
 #Optimizer
 optimizer_train=torch.optim.Adam(model.parameters(),lr=learning_rate)
-
+# optimizer_train=torch.optim.Adam(cifar_train.parameters(),lr=learning_rate)
 #Setting Steps
 total_train_step=0
 total_test_step=0
 
 #Rounds for training
-epoch=10
+epoch=20
 
 #data visualize: TensorBoard
 writer=SummaryWriter("./logs_cifar")
@@ -139,15 +151,15 @@ for i in range(epoch):
     final_true=[]
     #start training
     for j,data in enumerate(train_loader):
-        imgs,targets,l=data
-        imgs=imgs.reshape(20,3,74,74)  #batch,channel,img.height, img.weight
+        imgs,targets=data
+        # imgs=imgs.reshape(20,3,74,74)  #batch,channel,img.height, img.weight
         outputs=model(imgs)
         # print(f"outputs: {outputs}")
         # print(f"targets: {targets}")
 
         #Add class weight to loss function
         loss=loss_func(outputs,targets.long())
-        print(f"loss={loss}")
+        # print(f"loss={loss}")
 
         #optimizer
         optimizer_train.zero_grad()
@@ -156,7 +168,7 @@ for i in range(epoch):
         total_train_step=total_train_step+1
         if total_train_step%100==0:
             print("Train step {}, Loss: {}".format(total_train_step,loss))
-            writer.add_scalar("train_loss",loss.item(),total_train_step)
+            # writer.add_scalar("train_loss",loss.item(),total_train_step)
 
     #start testing
     # Create model
@@ -164,10 +176,11 @@ for i in range(epoch):
     # print(f"target_name= {target_name}")
     with torch.no_grad():    #ensure this data would not be optimized
         for l,data in enumerate(test_loader):
-            imgs,targets,k=data
+            imgs,targets=data
             imgs = imgs.reshape(20,3,74,74)
 
             outputs=model(imgs)
+            # outputs = cifar_train(imgs)
 
             loss=loss_func(outputs,targets.long())
             outputs_np=outputs.numpy()
